7dabd07a0e98f481ea15a8bb79e9e5cdc9563238
JoshSerbus09 <JoshSerbus09@gmail.com>
2013-01-20 20:13:57 -0600

    starting comments work

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
-class comment < ActiveRecord::Base
-	belongs_to :post
-	attr_accessible :text, :commenter
+class Comment < ActiveRecord::Base
+  belongs_to :post
+  attr_accessible :body, :commenter
 
-	validates_presence_of :name, :comment
+  validates_presence_of :body, :commenter
+  validates_length_of :body, :minimum => 10, :maximum => 160
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
-  attr_accessible :message, :name, :title
+  attr_accessible :message, :name, :title, :comments
 
   validates_presence_of :name, :title
   validates_uniqueness_of :title, :message
   validates_length_of :message, :minimum => 15, :maximum => 160
+
+  has_many :comments, :dependent => :destroy
 end
